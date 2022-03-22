21ab8926c73c918a71eb1e313c18e8d3d774bc37
Banshezzz <86187265+Banshezzz@users.noreply.github.com>
2022-03-22 22:19:26 +0700

    added api

--- api/index.js
+++ api/index.js
@@ -5,16 +5,11 @@ const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const movieRoute = require("./routes/movies");
-const listRoute = require("./routes/lists");
+// const listRoute = require("./routes/lists");
 
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("DB Connection Successfull"))
   .catch((err) => {
     console.error(err);
@@ -25,7 +20,7 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/movies", movieRoute);
-app.use("/api/lists", listRoute);
+// app.use("/api/lists", listRoute);
 
 app.listen(8800, () => {
   console.log("Backend server is running!");
