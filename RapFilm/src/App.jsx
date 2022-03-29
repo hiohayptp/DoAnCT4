5978d11a88a27d5582a9758c9d3c76ad1b26df75
HongSon99673 <hongson99673@gmail.com>
2022-03-29 14:25:43 +0700

    hoan thanh back end

--- RapFilm/src/App.jsx
+++ RapFilm/src/App.jsx
@@ -9,9 +9,11 @@ import Home from "./pages/home/Home";
 import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
 import Login from "./pages/login/Login";
+import { useContext } from "react";
+import{ AuthContext} from "./authContext/AuthContext"
 
 const App = () => {
-  const user = true;
+  const { user } = useContext(AuthContext);
   return (
     <Router>
       <Routes>
--- RapFilm/src/pages/register/Register.jsx
+++ RapFilm/src/pages/register/Register.jsx
@@ -1,19 +1,33 @@
+import axios from "axios";
 import { useRef } from "react";
 import { useState } from "react";
 import "./register.scss";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
 
   const emailRef = useRef();
   const passwordRef = useRef();
+  const usernameRef = useRef();
 
   const handleStart = () => {
     setEmail(emailRef.current.value);
   };
-  const handleFinish = () => {
+  const handleFinish = async (e) => {
+    e.preventDefault();
     setPassword(passwordRef.current.value);
+    setUsername(usernameRef.current.value);
+    try {
+      await axios.post("auth/register", { email, username, password});
+      navigate('/login');
+      console.log(email, password);
+    } catch (error) {
+      
+    }
   };
   return (
     <div className="register">
@@ -42,6 +56,7 @@ export default function Register() {
           </div>
         ) : (
           <form className="input">
+             <input type="username" placeholder="username" ref={usernameRef} />
             <input type="password" placeholder="password" ref={passwordRef} />
             <button className="registerButton" onClick={handleFinish}>
               Start
